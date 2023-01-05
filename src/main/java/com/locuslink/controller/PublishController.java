d4cd41674251d3250cd7c791c6f56dd1bd73b316
clayton sparks <claytonsparks803@gmail.com>
2023-01-05 16:25:40 -0500

    more pages

---
+++ src/main/java/com/locuslink/controller/PublishController.java
@@ -0,0 +1,50 @@
+package com.locuslink.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.locuslink.common.SecurityContextManager;
+import com.locuslink.dto.DashboardFormDTO;
+/**
+ * This is a Spring MVC Controller.
+ *
+ * @author C.Sparks
+ * @since 1.0.0 - January 05, 2023 - Initial version
+ */
+@Controller
+@Service
+public class PublishController {
+
+	private static final Logger logger = Logger.getLogger(PublishController.class);
+
+
+    @Value("${app.logout.url}")
+    private String appLogoutUrl;
+
+	// 6-2-2022 C.Sparks
+    @Autowired
+    private SecurityContextManager securityContextManager;
+
+
+
+	@PostMapping(value = "/initPublish")
+	public String initPublish (@ModelAttribute(name = "dashboardFormDTO") DashboardFormDTO dashboardFormDTO,	Model model, HttpSession session) {
+		logger.debug("Starting initPublish()...");
+
+	   	model.addAttribute("dashboardFormDTO", dashboardFormDTO);
+
+		return "fragments/publish";
+	}
+
+
+
+
+}
