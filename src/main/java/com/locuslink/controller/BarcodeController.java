6526380358d2834da066db9484a5b48b4b9eabd0
clayton sparks <claytonsparks803@gmail.com>
2024-01-12 13:57:17 -0500

    u

--- src/main/java/com/locuslink/controller/BarcodeController.java
+++ src/main/java/com/locuslink/controller/BarcodeController.java
@@ -82,9 +82,10 @@ public class BarcodeController {
 		
 		
 		
-		// 5-9-2023
-		String encodedPDFBarcdeString = bartenderRestClient.getBarcode_PDFEncodedStream(uniqueAssetDTO);		
-
+		// 1-13-2024 testing
+//		String encodedPDFBarcdeString = bartenderRestClient.getBarcode_PDFEncodedStream(uniqueAssetDTO);		
+		String encodedPDFBarcdeString = "";
+		
 	   	model.addAttribute("encodedPDFBarcdeString", encodedPDFBarcdeString);	
 		
 	   	model.addAttribute("uniqueAssetDTO", uniqueAssetDTO);		
