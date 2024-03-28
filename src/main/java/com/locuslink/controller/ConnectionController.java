d89c65c37fc92247b5adbf3f4ab57dd99401efbe
clayton sparks <claytonsparks803@gmail.com>
2024-03-28 15:34:43 -0400

    added connection routing

---
+++ src/main/java/com/locuslink/controller/ConnectionController.java
@@ -0,0 +1,62 @@
+package com.locuslink.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
+import org.codehaus.jettison.json.JSONObject;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.locuslink.common.GenericMessageResponse;
+import com.locuslink.dto.DashboardFormDTO;
+import com.locuslink.dto.OpenAI.ChatCompletionRequest;
+import com.locuslink.dto.OpenAI.ChatCompletionResponse;
+
+
+
+/**
+ * This is a Spring MVC Controller.
+ * 
+ * @author C.Sparks
+ * @since 1.0.0 - October 9, 2018 - Initial version
+ */
+@Controller
+@Service
+public class ConnectionController {
+ 
+	private static final Logger logger = Logger.getLogger(ProfileController.class);
+	
+	
+	@PostMapping(value = "initConnection")
+	public String initConnection (@ModelAttribute(name = "dashboardFormDTO") DashboardFormDTO dashboardFormDTO,	Model model, HttpSession session) {
+		
+		logger.debug("Starting initConnection()...");
+
+	   	model.addAttribute("dashboardFormDTO", dashboardFormDTO);
+
+	   	return "fragments/connection";
+	}
+	
+	
+	@PostMapping(value = "addConnection")
+	public String addConnection (@ModelAttribute(name = "dashboardFormDTO") DashboardFormDTO dashboardFormDTO,	Model model, HttpSession session) {
+		
+		logger.debug("Starting addConnection()...");
+
+	   	model.addAttribute("dashboardFormDTO", dashboardFormDTO);
+
+	   	return "fragments/connection-add";
+	}
+	
+
+}
